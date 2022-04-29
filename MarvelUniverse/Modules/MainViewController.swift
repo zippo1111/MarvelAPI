0b76c4fc08bf1b9b2d87b6bd76079d8009898651
AndreyK <zippo1111@ya.ru>
2022-04-29 21:47:43 +0300

    make initial setup for layout in code

--- MarvelUniverse/Modules/MainViewController.swift
+++ MarvelUniverse/Modules/MainViewController.swift
@@ -1,5 +1,5 @@
 //
-//  ViewController.swift
+//  MainViewController.swift
 //  MarvelUniverse
 //
 //  Created by Mangust on 29.04.2022.
@@ -7,11 +7,12 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class MainViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+
+        
     }
 
 
